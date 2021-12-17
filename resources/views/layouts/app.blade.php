45d49a0022863b935faf3c61a1526f7d08b26c78
FreshDev <billyhans90@gmail.com>
2021-12-17 04:49:52 +0100

    Add components to display the messages from the payment platform

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    @stack('styles')
 </head>
 
 <body>
@@ -80,6 +82,31 @@
         </nav>
 
         <main class="py-4">
+            <div class="container">
+            @if (isset($errors) && $errors->any())
+            <div class="alert alert-danger" role="alert">
+                <ul>
+                    @foreach ($errors->all() as $error )
+                    <li>{{ $error }}</li>
+                    @endforeach
+
+                </ul>
+                {{ session('status') }}
+            </div>
+            @endif
+
+            @if (session()->has('success'))
+            <div class="alert alert-success" role="alert">
+                <ul>
+                    @foreach (session()->get('success') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+
+                </ul>
+                {{ session('status') }}
+            </div>
+            @endif
+            </div>
             @yield('content')
         </main>
     </div>
@@ -92,6 +119,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <script src="{{ asset('js/app.js') }}"></script>
+
+    @stack('scripts')
 
 
 
