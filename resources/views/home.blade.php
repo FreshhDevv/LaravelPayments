1d9d00210ff3c7be51c9a81a161c7be53f122eac
FreshDev <billyhans90@gmail.com>
2021-12-18 21:17:08 +0100

    Create a form with random amounts using bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,22 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Make a Payment') }}</div>
 
                 <div class="card-body">
-                   
+                <form action="#" method="POST" id="paymentForm">
+                    @csrf
+                    <div class="row">
+                        <div class="col-auto">
+                            <label>How much do you want to pay?</label>
+                            <input type="number" min="5" step="0.01" class="form-control" name="value" value="{{ mt_rand(500, 100000) /100 }}">
+                        </div>
+                    </div>
+                    <div class="text-center" mt-3>
+                        <button type="submit" id="payButton" class="btn btn-primary btn-lg">Pay</button>
 
-                    {{ __('You are logged in!') }}
+                    </div>
+                </form>
                 </div>
             </div>
         </div>
